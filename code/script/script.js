bf744403c108f2c32c14d456ba2bb2f48faa2b71
theenaris <theenaris.p@gmail.com>
2025-07-20 22:17:27 +0700

    up date

---
+++ code/script/script.js
@@ -0,0 +1,11 @@
+fetch('/code/data/style/json/style of home page.json')
+  .then(res => res.json())
+  .then(style => {
+    const el = document.getElementById("LogoSunchineSalonFormatDivPageHome");
+
+    el.style.backgroundColor = style["background-color"];
+    el.style.color = style["color"];
+    el.style.fontStyle = style["text-style"];
+    el.style.fontWeight = style["text-bold"];
+    el.style.fontFamily = style["text-fmly"];
+  });
