1363837eb6640100261e505b6047c16db3a8ecff
theenaris1 <theeenaris@icloud.com>
2025-07-21 13:53:51 +0700

    hello

---
+++ code/script/pull json file.js
@@ -0,0 +1,14 @@
+fetch("/code/data/json/setting.json")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Settings loaded:', data);
+    // Use the data here
+  })
+  .catch(error => {
+    console.error('Error loading settings:', error);
+  });
